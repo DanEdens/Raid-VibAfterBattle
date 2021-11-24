641874c6afc6d6801e4ffb2abb472cf971077693
John Edens <dan.edens@geo-instruments.com>
2021-11-24 12:54:58 -0600

    adds apk and install script

---
+++ install.cmd
@@ -0,0 +1,5 @@
+@rem adb install -r APK/Raid_VibAfterBattle.6.apk
+@rem adb shell pm grant edens.buttsoap.raidvibafterbattle android.permission.SYSTEM_ALERT_WINDOW
+adb -s R38R502GTBP install -r APK/Raid_VibAfterBattle.6.apk
+adb -s R38R502GTBP shell pm grant edens.buttsoap.raidvibafterbattle android.permission.SYSTEM_ALERT_WINDOW
+
